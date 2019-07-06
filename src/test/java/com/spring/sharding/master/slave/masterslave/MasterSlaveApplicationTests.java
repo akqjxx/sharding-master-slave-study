5bd444c26d853ca53f331ef12ab4e7eef443c607
akqjxx <liuer2595919@163.com>
2019-07-06 19:28:49 +0800

    Initial commit

---
+++ src/test/java/com/spring/sharding/master/slave/masterslave/MasterSlaveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.sharding.master.slave.masterslave;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterSlaveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
